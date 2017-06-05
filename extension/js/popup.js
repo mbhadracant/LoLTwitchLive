565682f828e88b50faea44b7950f605866ef7372
mbhadracant <mbhadracant@gmail.com>
2017-06-05 22:56:38 +0100

    using mongoDB to store data and removed content scripts

---
+++ extension/js/popup.js
@@ -0,0 +1,52 @@
+$(function() {
+  chrome.tabs.query({
+    'active': true,
+    'lastFocusedWindow': true
+  }, function(tabs) {
+    var url = tabs[0].url;
+    var pattern = new RegExp("^https:\/\/www.twitch.tv\/\S*");
+    var valid = pattern.test(url);
+
+    if (!valid) {
+      $("#message").html("The current tab's URL is not a valid twitch page. Open up a twitch streaming page!")
+    } else {
+      var streamName = url.substring(url.lastIndexOf("/") + 1, url.length);
+      $("#message").html(streamName);
+      $("#message").css("font-weight", "bold");
+      $("#message").css("font-size", "14px");
+    }
+
+  });
+
+  $("#add-streamer").click(function() {
+    $("#home").fadeOut(function() {
+      $("#request").fadeIn();
+    });
+  });
+
+  $("#request form").on('submit', function(e) {
+    $("#request form").fadeOut(function() {
+      $("#content").append("<div class='success'>The streamer/summoner info has been submitted sucessfully. It will be validated and added onto the database if valid.</div>");
+    });
+  });
+
+  $.ajax({
+    url: "http://localhost:8080",
+    success: function(result) {
+      var twitchNames = [];
+
+      for (var i = 0; i < result.length; i++) {
+        twitchNames.push(result[i].twitchName);
+      }
+      $.typeahead({
+        input: '.js-typeahead-twitch-name',
+        order: "desc",
+        maxItem: 5,
+        source: {
+          data: twitchNames
+        }
+      });
+    }
+  });
+
+});
--- server/server.js
+++ server/server.js
@@ -2,113 +2,111 @@ var restify = require('restify');
 var request = require('request');
 var config = require('./config');
 var server = restify.createServer();
-var streamers = {
-  'froggen': {'summonerNames': [ {region: 'na', name: 'Anivia Kid'} ]},
-  'imaqtpie': {'summonerNames': [ {region: 'na', name: 'Imaqtpie'}, {region: 'na', name: 'feed l0rd'}, {region: 'na', name: 'kek'} ]},
-  'rush': {'summonerNames': [ {region: 'kr', name: 'Tiltlord'}, {region: 'kr', name: 'C9 Rush'} ]},
-  'pobelter': {'summonerNames': [ {region: 'na', name: 'Pobelter'} ]},
-  'IMT_WildTurtle': {'summonerNames': [ {region: 'na', name: 'WildTurtle'}, {region: 'na', name: 'Turtle The Cat'} ]},
-};
+server.use(restify.bodyParser({
+  mapParams: false
+}));
 
+var MongoClient = require('mongodb').MongoClient;
 
+// Connection URL
+var url = 'mongodb://localhost:27017/live_stream_info';
+var collection;
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  console.log("Connected successfully to server");
+  collection = db.collection('stream');
+  serverInit();
+  setInterval(function() {
+    var liveStreamers = findLiveStreamers();
+  }, 3000);
+});
 
-function init() {
-  initServer();
-  initSummonerIds();
-}
-
-function initServer() {
-  server.get('/stream/:streamerName', getStreamerData);
-  server.get('/', getAllStreamerData);
+function serverInit() {
+  server.get('/', home);
+  server.post('/add', add);
   server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
   });
 }
 
-function initSummonerIds() {
-  for(streamName in streamers) {
-    var summonerNames = streamers[streamName]['summonerNames'];
-    for(var i = 0; i < summonerNames.length; i++) {
-      var summonerName = summonerNames[i];
-      setSummonerId(streamName, summonerName, i);
-    }
-  }
-}
-
-function setSummonerId(streamName, summonerName, index) {
-  request(`https://${summonerName.region}.api.pvp.net/api/lol/${summonerName.region}/v1.4/summoner/by-name/${summonerName.name}?api_key=${config.RIOT_API_KEY}`,
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          var summonerNameKey = summonerName.name.toLowerCase().replace(/\s+/g, '');
-          streamers[streamName].summonerNames[index]['id'] = JSON.parse(body)[summonerNameKey]['id'];
-      }
+function home(req, res, next) {
+  collection.find().toArray(function(err, docs) {
+    res.send(docs);
+    next();
   });
 }
 
-function update() {
-  for(streamName in streamers) {
-    setStreamerData(streamName);
-  }
-}
+function add(req, res, next) {
 
-function setStreamerData(streamName) {
-  request(`https://api.twitch.tv/kraken/streams/${streamName}?client_id=${config.TWITCH_CLIENT_ID}`,
- function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var isStreamerOnline = (JSON.parse(body)['stream'] == null) ? false : true;
-    streamers[streamName]['isOnline'] = isStreamerOnline;
 
-    if(isStreamerOnline == false) return;
-
-    var summonerNames = streamers[streamName]['summonerNames'];
-    for(var i = 0; i < summonerNames.length; i++) {
-      var summoner = summonerNames[i];
-        setLiveGameDataIfFound(streamName, summoner);
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var id = JSON.parse(body)['id'];
+      console.log()
+      var obj = {
+        'id': id,
+        'summonerName': req.body.summonerName,
+        'region': req.body.region
+      };
+      collection.find({
+        'twitchName': req.body.twitchName
+      }).toArray(function(err, docs) {
+        if (docs.length >= 1) {
+          collection.updateOne({
+            'twitchName': req.body.twitchName
+          }, {
+            $push: {
+              'summoners': obj
+            }
+          });
+        } else {
+          var doc = {};
+          doc['twitchName'] = req.body.twitchName;
+          doc['summoners'] = [];
+          doc['summoners'].push(obj);
+          collection.insertOne(doc);
+        }
+        res.send("success");
+        next();
+      });
     }
-
   }
-})
+
+  request(`https://${req.body.region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.body.summonerName}?api_key=${config.RIOT_API_KEY}`, callback);
+
 }
 
-function setLiveGameDataIfFound(streamName, summoner) {
-  request(`https://${summoner.region}.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/${getRegionStringForCurrentGameCall(summoner.region)}/${summoner.id}?api_key=${config.RIOT_API_KEY}`,
-  function (error, response, body) {
-   if (!error && response.statusCode != 404) {
-     var data =  JSON.parse(body);
-     streamers[streamName]['liveGameData'] = data;
-   } else {
-     streamers[streamName]['liveGameData'] = undefined;
-   }
- });
+function getSummonerId(summoner) {
+
 }
 
-function getStreamerData(req, res, next) {
-  if(streamers[req.params.streamerName] == undefined){
-    return next(new restify.NotFoundError("Streamer not found"));
+function findLiveStreamers() {
+  collection.find().toArray(function(err, docs) {
+    for (var i = 0; i < docs.length; i++) {
+      var doc = docs[i];
+      var twitchName = doc['twitchName'];
+      checkStreamIsOnline(twitchName, doc);
+    }
+  });
+}
+
+function checkStreamIsOnline(twitchName, doc) {
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var isStreaming = (JSON.parse(body)['stream'] == null) ? false : true;
+      if (isStreaming) {
+        checkIfStreamerIsPlaying(twitchName, doc);
+      }
+    }
   }
-  res.send(streamers[req.params.streamerName]);
-  next();
+  request(`https://api.twitch.tv/kraken/streams/${twitchName}?client_id=${config.TWITCH_CLIENT_ID}`, callback);
 }
 
-function getAllStreamerData(req, res, next) {
-  res.send(streamers);
-  next();
-}
+function checkIfStreamerIsPlaying(twitchName, doc) {
+  var summoners = doc['summoners'];
+  for (var i = 0; i < summoners.length; i++) {
+    var summoner = summoners[i];
 
-function getRegionStringForCurrentGameCall(region) {
-  switch(region) {
-    case 'eu':
-      return 'EUW1';
-      break;
-    case 'na':
-      return 'NA1';
-      break;
-    case 'kr':
-      return 'KR';
-      break;
+    console.log(summoner['id'] + " - " + summoner['summonerName']);
   }
 }
-
-init();
-update();
-setInterval(update, 10000);
