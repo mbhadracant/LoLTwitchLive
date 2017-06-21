efb58dd52e58f79f0e661c78376e4a90d53e15ab
mbhadracant <mbhadracant@gmail.com>
2017-06-21 01:21:26 +0100

    using wrappers for APIs instead of request

--- server/server.js
+++ server/server.js
@@ -1,20 +1,34 @@
-var restify = require('restify');
-var request = require('request');
 var config = require('./config');
+var restify = require('restify');
+var LolApi = require('leagueapi');
+
+LolApi.init(config.RIOT_API_KEY,'euw');
+LolApi.setRateLimit(10, 3000);
+
 var server = restify.createServer();
+server.use(restify.queryParser());
 server.use(restify.bodyParser({
   mapParams: false
 }));
 
-var MongoClient = require('mongodb').MongoClient;
+var TwitchApi = require('twitch-api');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/live_stream_info';
+var twitch = new TwitchApi({
+    clientId: config.TWITCH_CLIENT_ID,
+    clientSecret: config.TWITCH_CLIENT_SECRET,
+    redirectUri: 'http://localhost.',
+    scopes: []
+});
+
+
+var MongoClient = require('mongodb').MongoClient;
 var collection;
-// Use connect method to connect to the server
+
+var url = 'mongodb://localhost:27017/live_stream_info';
 MongoClient.connect(url, function(err, db) {
   console.log("Connected successfully to server");
   collection = db.collection('stream');
+  collection.updateMany({}, {$set: {live: null}});
   serverInit();
   var liveStreamers = findLiveStreamers();
   setInterval(function() {
@@ -25,6 +39,8 @@ MongoClient.connect(url, function(err, db) {
 function serverInit() {
   server.get('/', home);
   server.post('/add', add);
+  server.get('/:twitchName', getLiveData);
+  server.get('/summoners/:twitchName', getSummoners);
   server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
   });
@@ -38,10 +54,12 @@ function home(req, res, next) {
 }
 
 function add(req, res, next) {
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var id = JSON.parse(body)['id'];
-      console.log()
+
+  LolApi.Summoner.getByName(req.body.summonerName, req.body.region, function(err, summoner) {
+  	if(!err) {
+      var name = Object.keys(summoner)[0];
+      var id = summoner[name]['id'];
+
       var obj = {
         'id': id,
         'summonerName': req.body.summonerName,
@@ -68,11 +86,39 @@ function add(req, res, next) {
         res.send("success");
         next();
       });
-    }
-  }
-
-  request(`https://${req.body.region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.body.summonerName}?api_key=${config.RIOT_API_KEY}`, callback);
+  	}
+  });
 }
+
+
+
+function getSummoners(req, res, next) {
+
+  var twitchName = req.params['twitchName'];
+
+
+  collection.findOne({twitchName : twitchName}, function(err, item) {
+          if(item == null) {
+            return next(new restify.BadRequestError("Cannot find the following streamer: " + twitchName));
+          }
+          res.send(item['summoners']);
+  });
+}
+
+function getLiveData(req, res, next) {
+  var twitchName = req.params['twitchName'];
+
+  collection.findOne({twitchName : twitchName}, function(err, item) {
+          if(item == null) {
+            return next(new restify.BadRequestError("Cannot find the following streamer: " + twitchName));
+          }
+          if(item['live'] == null) {
+            return next(new restify.ResourceNotFoundError("No live game data found for the streamer: " + twitchName));
+          }
+          res.send(item['live']);
+  });
+}
+
 
 function findLiveStreamers() {
   collection.find().toArray(function(err, docs) {
@@ -85,15 +131,12 @@ function findLiveStreamers() {
 }
 
 function checkStreamIsOnline(twitchName, doc) {
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var isStreaming = (JSON.parse(body)['stream'] == null) ? false : true;
-      if (isStreaming) {
-        checkIfStreamerIsPlaying(twitchName, doc);
-      }
+  twitch.getChannelStream(twitchName, function(err, data) {
+    var isStreaming = (data['stream'] == null) ? false : true;
+    if (isStreaming) {
+      checkIfStreamerIsPlaying(twitchName, doc);
     }
-  }
-  request(`https://api.twitch.tv/kraken/streams/${twitchName}?client_id=${config.TWITCH_CLIENT_ID}`, callback);
+  });
 }
 
 function checkIfStreamerIsPlaying(twitchName, doc) {
@@ -106,16 +149,86 @@ function checkIfStreamerIsPlaying(twitchName, doc) {
 }
 
 function setLiveDataIfFound(twitchName, summoner) {
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-
-        console.log("YAY - " + summoner['summonerName']);
-    } else if(!error && response.statusCode == 503) {
-        console.log("503 - " + summoner['summonerName']);
+  LolApi.getCurrentGame(summoner.id, summoner.region, function(err, data) {
+    if(!err) {
+      collection.findOne({twitchName : twitchName}, function(err, item) {
+              if(item['live'] == null) {
+                collection.updateOne({'twitchName':twitchName},{$set: {'live':data}}, null, function(err, r) {
+                  setLiveData(twitchName, summoner.region);
+                });
+              } else {
+                console.log('already has live data - ' + summoner['summonerName']);
+              }
+      });
+      console.log("YAY - " + summoner['summonerName']);
+    } else {
+      collection.updateOne({'twitchName':twitchName},{$set: {'live':null}});
+      console.log("error - " + summoner['summonerName']);
     }
-  }
-  request(`https://${summoner['region']}.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${summoner['id']}?api_key=${config.RIOT_API_KEY}`, callback);
+  });
+}
 
+function setLiveData(twitchName, region) {
+
+  function setVersion() {
+
+    LolApi.Static.getVersions(region,function(err,data) {
+      if(!err) {
+        collection.updateOne({'twitchName':twitchName},{$set: {'live.version':data[0]}}, null, function(err, r) {
+          setImages();
+        });
+      }
+    });
+
+  }
+
+  function setImages() {
+
+
+    collection.findOne({twitchName : twitchName}, function(err, item) {
+      if(item['live'] == null) {
+        return;
+      }
+
+      var participants = item['live']['participants'];
+      for(var i = 0; i < participants.length; i++) {
+        var player = participants[i];
+        var championId = player['championId'];
+        var spellId1 = player['spell1Id'];
+        var spellId2 = player['spell2Id'];
+
+        function setChampionImage(index, championId) {
+          var options = {champData: 'image', version: item['live']['version'], locale: 'en_US'}
+          LolApi.Static.getChampionById(championId, options, region, function(err, data) {
+
+            var filename = data['image']['full'];
+            var setObject = {};
+            setObject['live.participants.' + index + '.championImage'] = `http://ddragon.leagueoflegends.com/cdn/${options.version}/img/champion/${filename}`;
+            collection.updateOne({'twitchName':twitchName},{$set: setObject});
+          });
+        }
+
+        function setSpellsImage(index, spellId, spellNumber) {
+
+          var options = {spellData: 'image', version : item['live']['version'], locale: 'en_US'}
+
+
+          LolApi.Static.getSummonerSpellById(spellId, options, function(err, data) {
+            var filename = data['image']['full'];
+            var setObject = {};
+            setObject['live.participants.' + index + '.spellImage' + spellNumber] = `http://ddragon.leagueoflegends.com/cdn/${options.version}/img/spell/${filename}`;
+            collection.updateOne({'twitchName':twitchName},{$set: setObject});
+          });
+        }
+
+        setChampionImage(i, championId);
+        setSpellsImage(i, spellId1, 1);
+        setSpellsImage(i, spellId2, 2);
+      }
+    });
+  }
+
+  setVersion();
 }
 
 //db.lul.update({name:"Mayur"}, {$set: {kek:'xda'}});
